f6c81ad69f9d5310bacfe434bfaaa24c4667f9e3
DamWarczyk <foxplaygames81@gmail.com>
2022-05-08 17:18:37 +0200

    Backend for repository "Strona"

---
+++ TestBackend/src/main/java/com/example/TestBackend/StudentResource.java
@@ -0,0 +1,55 @@
+package com.example.TestBackend;
+
+import com.example.TestBackend.model.Student;
+import com.example.TestBackend.service.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+public class StudentResource {
+    private final StudentService studentService;
+
+    public StudentResource(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/all")
+    @CrossOrigin("http://localhost:4200")
+    public ResponseEntity<List<Student>> getAllStudents(){
+        List<Student> students = studentService.findAllStudents();
+        return new ResponseEntity<>(students, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    @CrossOrigin("http://localhost:4200")
+    public ResponseEntity<Student> getStudentsById(@PathVariable("id") Long id){
+        Student students = studentService.findStudentById(id);
+        return new ResponseEntity<>(students, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    @CrossOrigin("http://localhost:4200")
+    public ResponseEntity<Student> postStudent( @RequestBody Student student){
+        Student student1 = studentService.addStudent(student);
+        return new ResponseEntity<>(student1, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    @CrossOrigin("http://localhost:4200")
+    public ResponseEntity<Student> updateStudent( @RequestBody Student student){
+        Student updateStudent = studentService.addStudent(student);
+        return new ResponseEntity<>(updateStudent, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @CrossOrigin("http://localhost:4200")
+    public ResponseEntity<?> deleteStudent( @PathVariable("id") Long id){
+        studentService.deleteStudent(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
---
+++ TestBackend/src/main/java/com/example/TestBackend/service/StudentService.java
@@ -0,0 +1,39 @@
+package com.example.TestBackend.service;
+
+import com.example.TestBackend.exepction.UsernNotFoundException;
+import com.example.TestBackend.model.Student;
+import com.example.TestBackend.repo.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+    private  final StudentRepo studentRepo;
+
+    @Autowired
+    public StudentService(StudentRepo studentRepo) {
+        this.studentRepo = studentRepo;
+    }
+
+    public Student addStudent(Student student){
+        return studentRepo.save(student);
+    }
+
+    public List<Student> findAllStudents(){
+        return studentRepo.findAll();
+    }
+
+    public Student updateStudent(Student student){
+        return studentRepo.save(student);
+    }
+
+    public Student findStudentById(Long id){
+        return studentRepo.findStudentById(id).orElseThrow(() -> new UsernNotFoundException("User by id: " + id + "Not found"));
+    }
+
+    public  void deleteStudent(Long id){
+        studentRepo.deleteStudentById(id);
+    }
+}
